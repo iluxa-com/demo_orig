5f81adcb030acbf9c0a9c7e8e115debe69e4788f
Leonid Shalupov <shalupov@diverse.org.ru>
2012-08-03 14:37:48 +0400

    change4

--- src/DemoMain.java
+++ src/DemoMain.java
@@ -2,6 +2,7 @@ public class DemoMain {
   public static void main(String[] args) {
     System.out.println(hello.namespace.getHelloString());
     System.out.println("Bonus");
+    System.out.println("Bonus2");
     MyFunction.M();
   }
 }
