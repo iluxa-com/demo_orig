94ef016e8c3471952f5fedee05d889498d5bb5c6
Sasha Zverev <planerist@gmail.com>
2013-05-24 21:46:12 +0400

    comments

--- src/DemoMain.java
+++ src/DemoMain.java
@@ -1,8 +1,17 @@
 public class DemoMain {
   public static void main(String[] args) {
     System.out.println(hello.namespace.getHelloString());
+
+
+    //before
+
+
     System.out.println("Bonus");
     System.out.println("Bonus2");
     MyFunction.M();
+
+
+    //     after
+
   }
 }
