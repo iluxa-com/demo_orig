5e6958de1ec6d2bd803b964ce935db3c08f59e40
Sasha Zverev <planerist@gmail.com>
2013-05-25 00:21:38 +0400

    New changelist

--- src/DemoMain.java
+++ src/DemoMain.java
@@ -2,7 +2,7 @@ public class DemoMain {
   public static void main(String[] args) {
     System.out.println(hello.namespace.getHelloString());
     //before1234567890
-    System.out.println("Bonus");
+    System.out.println("BonXXXXXXXus");
     // hello1
     // hello2
     // hello3
