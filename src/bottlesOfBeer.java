a92399d04971e74755abc91065fa2b4bb6067ae8
Sergey Kesarev <sergey.kesarev@jetbrains.com>
2014-02-26 20:25:25 +0400

    Oh, wait... I can see one more wall...

--- src/bottlesOfBeer.java
+++ src/bottlesOfBeer.java
@@ -2,7 +2,7 @@
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Oooooookey! That's all...");
+    System.out.println("Oh, wait... I can see one more wall...");
     return 0;
   }
 }
