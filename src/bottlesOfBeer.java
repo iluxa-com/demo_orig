24b3b35ab07a63ac59b1373a8f623a98212edbe7
Sergey Kesarev <sergey.kesarev@jetbrains.com>
2014-02-26 20:22:52 +0400

    Oooooookey! That's all...

--- src/bottlesOfBeer.java
+++ src/bottlesOfBeer.java
@@ -2,7 +2,7 @@
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("1 bottles of beer on the wall. 1 bottles of beer! Take one down, pass it around, 0 bottles of beer on the wall!");
+    System.out.println("Oooooookey! That's all...");
     return 0;
   }
 }
