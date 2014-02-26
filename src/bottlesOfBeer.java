c85d08339d02a10250df49c1d8e62d7a5a719204
Sergey Kesarev <sergey.kesarev@jetbrains.com>
2014-02-26 19:48:20 +0400

    I don't want to drink more beer.

--- src/bottlesOfBeer.java
+++ src/bottlesOfBeer.java
@@ -2,7 +2,7 @@
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("90 bottles of beer on the wall. 90 bottles of beer! Take one down, pass it around, 89 bottles of beer on the wall!");
+    System.out.println("Stop it!");
     return 0;
   }
 }
