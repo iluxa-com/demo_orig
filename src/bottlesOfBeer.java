3d09af1fcbbef7bb48c84052c3df9675b04f4936
Sergey Kesarev <sergey.kesarev@jetbrains.com>
2014-02-26 19:42:20 +0400

    99 bottles of beer on the wall. 99 bottles of beer! Take one down, pass it around, 98 bottles of beer on the wall

---
+++ src/bottlesOfBeer.java
@@ -0,0 +1,9 @@
+//Generate commits.
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("99 bottles of beer on the wall. 99 bottles of beer! Take one down, pass it around, 98 bottles of beer on the wall!");
+    return 0;
+  }
+}
+
