a6e4c3e2f1bc1937c5ac14013905383577593a9e
Sergey Kesarev <sergey.kesarev@jetbrains.com>
2014-02-26 20:21:54 +0400

    "72 bottles of beer on the wall. 72 bottles of beer! Take one down, pass it around, 71 bottles of beer on the wall!"

--- src/bottlesOfBeer.java
+++ src/bottlesOfBeer.java
@@ -2,7 +2,7 @@
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("73 bottles of beer on the wall. 73 bottles of beer! Take one down, pass it around, 72 bottles of beer on the wall!");
+    System.out.println("72 bottles of beer on the wall. 72 bottles of beer! Take one down, pass it around, 71 bottles of beer on the wall!");
     return 0;
   }
 }
